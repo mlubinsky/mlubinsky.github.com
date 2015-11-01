6288df3a0c7f75e58f6631e5605465db6b7af2e4
Michael <mlubinsky@hotmail.com>
2015-10-31 22:22:17 -0700

    ProducerConsumerBlockingQueue.java

---
+++ java/sync/ProducerConsumerBlockingQueue.java
@@ -0,0 +1,74 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+ 
+/**
+ * Producer Class.
+ */
+class Producer implements Runnable {
+ 
+    private final BlockingQueue<Integer> sharedQueue;
+ 
+    public Producer(BlockingQueue<Integer> sharedQueue) {
+        this.sharedQueue = sharedQueue;
+    }
+ 
+    @Override
+    public void run() {
+        for(int i=1; i<=10; i++){
+         try {
+             System.out.println("Produced : " + i);
+             //put/produce into sharedQueue.
+             sharedQueue.put(i);          
+         } catch (InterruptedException ex) {
+             
+         }
+        }
+    }
+ 
+}
+ 
+/**
+ * Consumer Class.
+ */
+class Consumer implements Runnable{
+ 
+    private BlockingQueue<Integer> sharedQueue;
+ 
+    public Consumer (BlockingQueue<Integer> sharedQueue) {
+        this.sharedQueue = sharedQueue;
+    }
+ 
+    @Override
+    public void run() {
+        while(true){
+         try {
+           //take/consume from sharedQueue.
+             System.out.println("CONSUMED : "+ sharedQueue.take());  
+         } catch (InterruptedException ex) {
+             
+         }
+        }
+    }
+ 
+ 
+}
+ 
+/** Copyright (c), AnkitMittal JavaMadeSoEasy.com */
+public class ProducerConsumerBlockingQueue {
+ 
+    public static void main(String args[]){
+     
+     //Creating shared object  
+     BlockingQueue<Integer> sharedQueue = new LinkedBlockingQueue<Integer>();
+    
+     Producer producer=new Producer(sharedQueue);
+     Consumer consumer=new Consumer(sharedQueue);
+    
+     Thread producerThread = new Thread(producer, "ProducerThread");
+     Thread consumerThread = new Thread(consumer, "ConsumerThread");
+     producerThread.start();
+     consumerThread.start();
+ 
+    }
+ 
+}
