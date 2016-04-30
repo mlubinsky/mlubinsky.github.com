03d5eebd1fd3322cf90587f197269e206fabbd4e
Michael Lubinsky <mlubinsky@hotmail.com>
2016-04-30 14:51:23 -0700

    examples of file download

--- Flask/downoad_csv.py
+++ Flask/downoad_csv.py
@@ -5,21 +5,34 @@ app = Flask(__name__)
 def hello():
     return '''
         <html><body>
-        Hello. <a href="/getPlotCSV">Click me to download file.</a>
+        Examples of file download in Flask
+        <br>
+        <a href="/getFile1">Download file by reading it and using Response().</a>
+        <br>
+        <a href="/getFile2">Download file <b> data/a.csv </b> by using send_file()</a>
         </body></html>
         '''
 
-@app.route("/getPlotCSV")
-def getPlotCSV():
+@app.route("/getFile1")
+def getFile1():
     # with open("outputs/Adjacency.csv") as fp:
     #     csv = fp.read()
-    csv = '1,2,3\n4,5,6\n'
-    
+    csv = '1,2,3\n4,5,6\n' # this is the cheating to eliminating the reading the file
+
     return Response(
         csv,
         mimetype="text/csv",
         headers={"Content-disposition":
                  "attachment; filename=myplot.csv"})
 
+# Another approach: use send_file
+from flask import send_file
+
+@app.route('/getFile2') # this is a job for GET, not POST
+def getFile2():
+    return send_file('data/a.csv',
+                     mimetype='text/csv',
+                    # attachment_filename='a.csv',
+                     as_attachment=True)
 
 app.run(debug=False, port=5001)
