d844e71c868bc1ac2359a0e05f95a16b919b3560
Michael Lubinsky <Michael Lubinsky>
2016-04-29 22:28:44 -0700

    updated bokeh example

--- Flask/bokeh_example.py
+++ Flask/bokeh_example.py
@@ -10,9 +10,12 @@ app = Flask(__name__)
 route = app.route
 
 template_dir = 'templates'
+HTML_file='generated_plot.html'
+
 if not os.path.exists(template_dir):
     os.mkdir(template_dir)
-plot_file = pjoin(template_dir, 'generated_plot.html')
+
+plot_file = pjoin(template_dir, HTML_file)
 
 @route('/')
 def plot():
@@ -25,9 +28,10 @@ def plot():
     html = file_html(p, CDN, "plot")
     with open(plot_file, 'w') as f:
         f.write(html)
-    return render_template('generated_plot.html')
+    return render_template(HTML_file)
 
 
 if __name__ == '__main__':
+    print "http://127.0.0.1:5000/"
     app.debug = True
     app.run()
