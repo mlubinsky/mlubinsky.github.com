df106a0c4529c0e00e6457f8f8ce013e3fbfabb3
Michael Lubinsky <mlubinsky@hotmail.com>
2017-02-23 21:51:19 -0800

    numpy

--- python/num.py
+++ python/num.py
@@ -1,5 +1,7 @@
 #  http://www.bogotobogo.com/python/python_numpy_matrix_tutorial.php
-# http://www.bogotobogo.com/python/python_numpy_array_tutorial_basic_B.php
+#  http://www.bogotobogo.com/python/python_numpy_array_tutorial_basic_B.php
+# https://blog.drskippy.com/2012/05/14/dimension-reduction-for-machine-learning-simple-example-of-svd-pca-pathology://blog.drskippy.com/2012/05/14/dimension-reduction-for-machine-learning-simple-example-of-svd-pca-pathology/
+
 import numpy as np
 from numpy.linalg import eig
 np.show_config()
@@ -14,11 +16,15 @@ print zero.shape
 print "----- ONE -----"
 one= np.ones((4,2))
 print one
+print "transpose"
+print one.transpose()
 
 print "----- DENTITY -----"
 i=np.eye(3)
 print i
 print i.shape
+print "transpose"
+print i.transpose()
 
 print "------  RAND -------"
 rand= np.random.random((2,2)) #in range 0-1
