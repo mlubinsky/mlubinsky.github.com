e9d5eb35181c2d03e97ebdf7648bf26668778688
Michael Lubinsky <mlubinsky@apple.com>
2017-03-10 18:30:55 -0800

    update

---
+++ linear_regression.py
@@ -0,0 +1,27 @@
+# y = x1 + 2*x2 + 3*x3
+
+from random import randint
+TRAIN_SET_LIMIT = 1000
+TRAIN_SET_COUNT = 100
+
+TRAIN_INPUT = list()
+TRAIN_OUTPUT = list()
+for i in range(TRAIN_SET_COUNT):
+    a = randint(0, TRAIN_SET_LIMIT)
+    b = randint(0, TRAIN_SET_LIMIT)
+    c = randint(0, TRAIN_SET_LIMIT)
+    op = a + (2*b) + (3*c)
+    TRAIN_INPUT.append([a, b, c])
+    TRAIN_OUTPUT.append(op)
+
+from sklearn.linear_model import LinearRegression
+
+predictor = LinearRegression(n_jobs=-1)
+predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
+
+X_TEST = [[10, 20, 30]]
+outcome = predictor.predict(X=X_TEST)
+coefficients = predictor.coef_
+
+print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
+
--- python/num.py
+++ python/num.py
@@ -3,7 +3,7 @@
 #  http://www.bogotobogo.com/python/python_numpy_matrix_tutorial.php
 #  http://www.bogotobogo.com/python/python_numpy_array_tutorial_basic_B.php
 # https://blog.drskippy.com/2012/05/14/dimension-reduction-for-machine-learning-simple-example-of-svd-pca-pathology://blog.drskippy.com/2012/05/14/dimension-reduction-for-machine-learning-simple-example-of-svd-pca-pathology/
-import time 
+import time
 import numpy as np
 from numpy.linalg import eig
 np.show_config()
@@ -16,13 +16,10 @@ print zero.shape
 
 print "----- ONE -----"
 one= np.ones((4,2))
- 
 
 one_int= np.ones((4,2), dtype=int)
 print one
 print one_int
-
-
 
 print "transpose"
 print one.transpose()
@@ -36,27 +33,15 @@ print i.shape
 print "Identity transpose"
 print i.transpose()
 
-print "------  RAND -------"
-rand= np.random.random((2,2)) #in range 0-1
+print "------  RAND in (0-1) interval -------"
+rand= np.random.random((20)) #in range 0-1
+print rand
 
-print "------  COL -------"
-col= np.array([1,2,3])
-print col
-print col.shape
-print col.size
-
-print "------  ROW -------"
-row= np.array([[1,2,3]])
-print row
-print row.shape
-print row.size
-
-
-
+print "-- Matrix "
 A = np.array([[1,2],[3,4]])
 print A
-print "sum=", A.sum(), A.sum(axis=0), A.sum(axis=1) 
-print "mean=", A.mean(), A.mean(axis=0),  A.mean(axis=1) 
+print "sum=", A.sum(), A.sum(axis=0), A.sum(axis=1)
+print "mean=", A.mean(), A.mean(axis=0),  A.mean(axis=1)
 print "min=", A.min()
 print "max=", A.max()
 print "std=", A.std()
@@ -70,21 +55,35 @@ print eig_vec
 
 print "--DOT PRODUCT--"
 
+print "------  COL -------"
+col= np.array([1,2,3])
+print col
+print "shape=", col.shape, " size=",  col.size
+print "inneri with himself =", col.dot(col)
+print "outer with himself=", np.outer(col,col)
+print "cross with himself=", np.cross(col,col)
+
+print "------  ROW -------"
+row= np.array([[1,2,3]])
+print row
+print "shape=", row.shape, " size=",  row.size
+print "transpose row ", row.T
+print "x*x.T ", row*row.T
+print "x.T * x ", row.T * row
+print "cross with himself=", np.cross(row,row)
+
 
 print "--INNER PRODUCT  for 1d vectors  is just a number (scalar product) "
 print np.inner(col,row)
 print np.inner(row,col)
 print np.inner(row,row)
 
-
 print "-- Cross product of vectors is a vector which is ortogonal to both vectors and length=|a| |b| sin(a,b)"
 print np.cross(col,row)
 print np.cross(row,col)
 
-
 print "----linspace-----"
 print np.linspace(0,10,5, endpoint=True)
 print np.linspace(0,10,5, endpoint=False)
-
 
 time.sleep(20)
