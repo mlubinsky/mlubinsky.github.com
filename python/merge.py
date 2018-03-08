2a081d68e8487c135d14d364e188e02764225056
Michael Lubinsky <mlubinsky@apple.com>
2018-03-08 12:17:17 -0800

    update

--- python/merge.py
+++ python/merge.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+import heapq
 
 def merge_sorted_lists(left, right):
     """
@@ -51,11 +52,49 @@ def merge_2_sorted_lists(left, right):
 
     return result
 
+###   merge any numbers of lists
 
+#return a new sorted merged list from K sorted lists, each with size N.
+#Initialize the heap. In Python this this is just a list.
+# We need K tuples.
+#     one for the index for which list among the list of lists the element lives;
+#     one for the element index which is where the element lives; and the value of the element.
+# Since we want the key of the heap to be based on the value of the element, we should put that first in the tuple.
+# The time complexity for this would be O(KN log K), since we remove and append to the heap K * N times.
+
+# while the heap is not empty we need to:
+#  extract the minimum element from the heap: (value, list index, element index)
+# if the element index is not at the last index, add the next tuple in the list index.
+
+
+def merge(lists):
+    merged_list = []
+    # pass to heap the tuples (1st element of every list, list index, element index)
+    heap = [(lst[0], i, 0) for i, lst in enumerate(lists) if lst]
+    heapq.heapify(heap)
+
+    while heap:
+        val, list_ind, element_ind = heapq.heappop(heap)
+
+        merged_list.append(val)
+
+        if element_ind + 1 < len(lists[list_ind]):
+            next_tuple = (lists[list_ind][element_ind + 1],
+                          list_ind,
+                          element_ind + 1)
+            heapq.heappush(heap, next_tuple)
+    return merged_list
 
 l1=[1,3,5]
 l2=[2,4,6]
 #l3=merge_sorted_lists(l1, l2)
 #print l3
-l4=merge_sorted_lists(l1, l2)
-print l4
+#l4=merge_sorted_lists(l1, l2)
+#print l4
+all_lists=[]
+all_lists.append(l1)
+all_lists.append(l2)
+print all_lists
+
+all_lists_sorted=merge(all_lists)
+print all_lists_sorted
