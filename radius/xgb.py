c53a194afd0835a2863779571f3880753e216f07
Michael Lubinsky <mlubinsky@hotmail.com>
2021-02-18 12:41:51 -0800

    Add files via upload

--- radius/xgb.py
+++ radius/xgb.py
@@ -17,12 +17,13 @@ from statsmodels.tsa.stattools import pacf, acf
 import datastore
 
 import xgboost
-from xgboost import plot_importance, plot_tree
-#import xgboost_autotune 
+#from xgboost import plot_importance, plot_tree
 
-import xgb_autotune 
+
+import xgb_autotune
 
 DEBUG=True
+# DEBUG=False
 #---------------
 def input_cmd():
 #---------------
@@ -34,22 +35,22 @@ def input_cmd():
 
     parser.add_argument("-c", "--company",
                         type=int,
+                        help="The company")
 
-                        help="The company")                    
+    parser.add_argument("-d", "--direction",
+                        type=int,
+                        default=0, choices=[0, 1,2],
+                        help="The traffic direction: 0-all; 1-in; 2-out")
 
-    ##parser.add_argument("-f", "--file", 
-    ##                    type=str,
-    ##                    help="The file with 2 columns: date and value")
-
-    parser.add_argument("-b", "--begin", 
+    parser.add_argument("-b", "--begin",
                         type=str,
                         help="The start date in format YYYY-MM-DD")
 
-    parser.add_argument("-e", "--end", 
+    parser.add_argument("-e", "--end",
                         type=str,
-                        help="The end date in format YYYY-MM-DD")                    
+                        help="The end date in format YYYY-MM-DD")
 
-    parser.add_argument("-t", "--table", 
+    parser.add_argument("-t", "--table",
                         type=str,
                         default="jangle_traffic_total", choices=['jangle_traffic_total', 'radius_traffic_total'],
                         help="The table")
@@ -73,7 +74,7 @@ def sql_hourly_traffic(table, start, end, company, direction=None):
   if not direction:
     filter="1=1"
   else:
-    filter="direction="+str(direction) 
+    filter="direction="+str(direction)
 
   SQL=f"""
    SELECT
@@ -91,14 +92,15 @@ def sql_hourly_traffic(table, start, end, company, direction=None):
 
   return SQL
 #-------------------------------------------------------------------------
-def get_data_from_db(table, start_date, end_date, company, direction=None):
+def get_data_from_db(table, start_date, end_date, company, direction):
 #-------------------------------------------------------------------------
 
   sql =  sql_hourly_traffic(table, start_date, end_date, company, direction)
   print(sql)
   df = query(sql)
   if df.empty:
-    return df
+    print("No data found")
+    exit(0)
 
   date_range  = pd.date_range(start=df["date"].min(), end=df["date"].max(), freq='H')
   df['date'] = pd.to_datetime(df['date'])
@@ -122,7 +124,7 @@ def generate_series(signal, start_day, number_of_days, amplitude=1, freq=1):
          const =  0
 
          s = pd.Series( const + amplitude * ( np.sin(rng.hour * freq * PI  /12.0 )  ), index=rng, name='y')
-   
+
      elif signal == "linear":
 
       first_timepoint=rng[0]
@@ -131,18 +133,18 @@ def generate_series(signal, start_day, number_of_days, amplitude=1, freq=1):
 
       hours_from_start =  (rng - first_timepoint).astype('timedelta64[h]').astype(int)
       s = pd.Series( intersect + slope * hours_from_start, index=rng, name="y" )
- 
+
      elif signal == "linear_and_sin":
- 
+
       first_timepoint=rng[0]
       intersect=0.0
       slope=1.0
       amplitude=10
       hours_from_start =  (rng - first_timepoint).astype('timedelta64[h]').astype(int)
-      s = pd.Series( 
-         intersect + slope * hours_from_start + amplitude * ( np.sin(rng.hour * freq * PI  /12.0 )  ), 
-         index=rng, 
-         name="y" 
+      s = pd.Series(
+         intersect + slope * hours_from_start + amplitude * ( np.sin(rng.hour * freq * PI  /12.0 )  ),
+         index=rng,
+         name="y"
       )
 
      else:
@@ -157,13 +159,13 @@ def generate_series(signal, start_day, number_of_days, amplitude=1, freq=1):
 
 #------------------------
 def detrend(s):
-#------------------------    
+#------------------------
     print("detrend")
-    y = s.values   
+    y = s.values
     l = len(s)
     X = np.array(list(range(0, l))).reshape(-1,1)
 
-   
+
     model=LinearRegression().fit(X , y )
     print('intercept:', model.intercept_)
     print('slope:', model.coef_)
@@ -173,7 +175,7 @@ def detrend(s):
        plt.plot(y)
        plt.plot(trend)
        plt.show()
-    
+
     new_s = pd.Series( s.values - trend, s.index)
 
     #print(new_s)
@@ -190,7 +192,7 @@ def detrend(s):
 
 #-----------------------------------------------
 def forecast_split(data, n_steps=5, freq="1H"):
-#-----------------------------------------------    
+#-----------------------------------------------
       """
        Returns
         t_target: pd.Series with the training data
@@ -249,7 +251,10 @@ def get_features(y, opt, f_lags=True, lags=None):
     return features, target
 #-----------------------------
 def create_ts_features(data, opt):
-#-----------------------------    
+#-----------------------------
+    #print(data)
+    #exit(1)
+
     def get_shift(row):
         """
         shift: 3 shifts per day of 8 hours
@@ -260,9 +265,9 @@ def create_ts_features(data, opt):
             return 3
         else:
             return 1
-    
+
     features = pd.DataFrame()
-    
+
     features["hour"] = data.index.hour
     features["weekday"] = data.index.weekday
     features["dayofyear"] = data.index.dayofyear
@@ -282,18 +287,28 @@ def create_ts_features(data, opt):
 
     if opt == "use_shift":
         features["shift"] = pd.Series(data.index.map(get_shift))
- 
- 
+
+    if opt ==  "use_lag_4":
+       features["Lag_1"]=data.shift(1)
+       features["Lag_2"]=data.shift(2)
+       features["Lag_3"]=data.shift(3)
+       features["Lag_4"]=data.shift(4)
+
+       features["Lag_1"].fillna(0, inplace=True)
+       features["Lag_2"].fillna(0, inplace=True)
+       features["Lag_3"].fillna(0, inplace=True)
+       features["Lag_4"].fillna(0, inplace=True)
+
     features.index = data.index
-        
+
     return features
 #----------------------------------------------------
 def create_lag_features(target, lags=None, thres=0.2):
-#----------------------------------------------------    
+#----------------------------------------------------
     print("--- lags=None  create_lag_features lags=", lags)
     scaler = StandardScaler()
     features = pd.DataFrame()
-                
+
     if lags is None:
         # PACF - partial autocorrelation functions.
         # consider lag features only when PACF is greater than 0.2 (5% relevance) to build the features
@@ -304,12 +319,12 @@ def create_lag_features(target, lags=None, thres=0.2):
         except:
             print("error numpy.linalg.LinAlgError: Singular matrix")
             #return pd.DataFrame()
-             
+
 
     df = pd.DataFrame()
 
     if not lags or len(lags) == 0 :
-                print("no lags")  
+                print("no lags")
                 return df
 
     if 0 in lags:
@@ -317,86 +332,86 @@ def create_lag_features(target, lags=None, thres=0.2):
     for l in lags:
         df[f"lag_{l}"] = target.shift(l)
 
-    print("----  lags= --------")        
+    print("----  lags= --------")
     print (lags)
     print("len df=",  len(df))
     #print(df.columns)
     #print("--------------")
 
-    features = pd.DataFrame(scaler.fit_transform(df[df.columns]), 
+    features = pd.DataFrame(scaler.fit_transform(df[df.columns]),
                             columns=df.columns)
 
     features = df
     features.index = target.index
-    
+
     return features
 
 #---------------------------------------------------------------
 def direct( y, train_fn, lags, n_steps, opt, step="1H",  params=None):
-#---------------------------------------------------------------        
+#---------------------------------------------------------------
         """Multi-step direct forecasting using a machine learning model
         to forecast each time period ahead
-        
+
         Parameters
         ----------
         y: pd.Series holding the input time-series to forecast
         train_fn: a function for training the model which returns as output the trained model
                   cross-validation score and test score
         params: additional parameters for the training function
-        
+
         Returns
         -------
-        fcast_values: pd.Series with forecasted values indexed by forecast horizon dates    
+        fcast_values: pd.Series with forecasted values indexed by forecast horizon dates
         """
-        
+
         def one_step_features(date, step):
 
-            # features must be obtained using data lagged 
+            # features must be obtained using data lagged
             # by the desired number of steps (the for loop index)
-            tmp = y[y.index <= date]       
+            tmp = y[y.index <= date]
             lags_features = create_lag_features(tmp, lags=lags)
             ts_features = create_ts_features(tmp, opt)
             if lags_features.empty:
                 print("-----  no lags--------")
-            
+
             features = ts_features.join(lags_features, how="outer").dropna()
-            #else:    
+            #else:
             #    features = ts_features.copy()
 
-            # build target to be ahead of the features built 
+            # build target to be ahead of the features built
             # by the desired number of steps (the for loop index)
             target = y[y.index >= features.index[0] + pd.Timedelta(hours=step)]
             assert len(features.index) == len(target.index)
-            
+
             return features, target
-            
+
         params = {} if params is None else params
         fcast_values = []
-        fcast_range = pd.date_range(y.index[-1] + pd.Timedelta(hours=1), 
-                                    periods=n_steps, #   self.n_steps, 
+        fcast_range = pd.date_range(y.index[-1] + pd.Timedelta(hours=1),
+                                    periods=n_steps, #   self.n_steps,
                                     freq=step # self.step
         )
         fcast_features, _ = one_step_features(y.index[-1], 0)
-                
+
         for s in range(1, n_steps+1):  # self.n_steps+1):
-            
+
             last_date = y.index[-1] - pd.Timedelta(hours=s)
             features, target = one_step_features(last_date, s)
-            
-            model, cv_score, test_score = train_fn(features, target, **params) 
-                
+
+            model, cv_score, test_score = train_fn(features, target, **params)
+
             # use the model to predict s steps ahead
-            predictions = model.predict(fcast_features)        
+            predictions = model.predict(fcast_features)
             fcast_values.append(predictions[-1])
-                    
-        return pd.Series(index=fcast_range, data=fcast_values)
+
+        return pd.Series(index=fcast_range, data=fcast_values), model
 
 
 
 # https://github.com/dmlc/xgboost/blob/master/demo/guide-python/custom_rmsle.py
 #--------------------------
 def rmsle(real, predicted):
-#--------------------------    
+#--------------------------
     sum=0.0
     for x in range(len(predicted)):
         if predicted[x]<0 or real[x]<0: #check for negative values
@@ -407,11 +422,11 @@ def rmsle(real, predicted):
     return (sum/len(predicted))**0.5
 
 #-------------------------------------------------------------------
-def train_autotune_model(model, X_train, y_train): 
+def train_autotune_model(model, X_train, y_train):
 #-------------------------------------------------------------------
      rmlse_score = make_scorer( rmsle, greater_is_better=False)
 
-     fitted_model = xgb_autotune.fit_parameters(    
+     fitted_model = xgb_autotune.fit_parameters(
         initial_model = xgboost.XGBRegressor(
             objective =  'reg:squarederror'
         ),
@@ -419,7 +434,7 @@ def train_autotune_model(model, X_train, y_train):
         X_train = X_train,
         y_train = y_train,
         min_loss = 0.01,
-        scoring = rmlse_score, 
+        scoring = rmlse_score,
         n_folds=5
      )
 
@@ -431,7 +446,7 @@ def train_autotune_model(model, X_train, y_train):
 
 #-------------------------------------------------------------------
 def xgboost_model(features, target, test_size=0.2, max_evals=10):
-#-------------------------------------------------------------------    
+#-------------------------------------------------------------------
     """
     Full training and testing pipeline for XGBoost ML model with
     hyperparameter optimization using Bayesian method
@@ -447,8 +462,8 @@ def xgboost_model(features, target, test_size=0.2, max_evals=10):
     test_score: the RMSE on the test set
     """
 
-    X_train, X_test, y_train, y_test = train_test_split(features, 
-                                                        target, 
+    X_train, X_test, y_train, y_test = train_test_split(features,
+                                                        target,
                                                         test_size=test_size,
                                                         shuffle=False)
 
@@ -457,7 +472,7 @@ def xgboost_model(features, target, test_size=0.2, max_evals=10):
     return model, 0.0, 0.0
 #------------------------------------
 def multi_direct(c_target, f_steps, option):
-#------------------------------------    
+#------------------------------------
     print("-- multi_direct() ---")
 
     t_target, f_target, fcast_range = forecast_split(c_target, n_steps=f_steps)
@@ -466,7 +481,7 @@ def multi_direct(c_target, f_steps, option):
     print("--- multi_direct() len(t_target) =", len(t_target), \
                                   "len(f_target)=", len(f_target), \
                                   "len(fcast_range)=", len(fcast_range) \
-    )                      
+    )
     #print("--- multi_direct() features---")
     #print(features.info())
     #print(features)
@@ -477,26 +492,29 @@ def multi_direct(c_target, f_steps, option):
 
     lags = [int(f.split("_")[1]) for f in features if "lag" in f]
     #print("--- multi_direct() main lags=", lags)
-    
+
     t_target_detrended, model_detrended = detrend(t_target)
     #fcast_xgb = direct(t_target, xgboost_model, lags, f_steps)
-    fcast_xgb_detrended = direct(t_target_detrended, xgboost_model, lags, f_steps, option)
+    fcast_xgb_detrended , model = direct(t_target_detrended, xgboost_model, lags, f_steps, option)
 
     start=len(t_target)
     end = start + len(f_target)
     X = np.array(list(range(start, end))).reshape(-1,1)
     fcast_xgb = model_detrended.predict(X) + fcast_xgb_detrended
-    fcast_xgb[fcast_xgb < 0] = 0 
- 
+    fcast_xgb[fcast_xgb < 0] = 0
+
 
     if DEBUG:
        try:
-         plot_importance(fcast_xgb_detrended, height=0.9)
+         print("before fcast_xgb_detrended.feature_importances_")
+         print(model.feature_importances_)
+         print("After cast_xgb_detrended.feature_importances_")
+         xgboost.plot_importance(model, height=0.9)
          plt.show()
        except:
          print("error in plot_importance()")
          print(sys.exc_info()[0])
-         #exit(1)
+         exit(1)
 
     return fcast_xgb
 #---------------------------------------------
@@ -508,9 +526,9 @@ def final_plot(c_target, predictions, options, header):
 
     for i, pred in enumerate(predictions):
          ax.plot(pred,  label=options[i], linestyle='-', marker='o')
-    
+
     #ax.plot(c_target, color="blue", label="Original", linestyle='-', marker='o')
-    
+
     #ax.plot(fcast_xgb, color="green", label="Direct forecast", linestyle='-', marker='o')
     #ax.set_title(f"{f_steps} hours forecast")
     ax.set_title(header)
@@ -518,14 +536,14 @@ def final_plot(c_target, predictions, options, header):
     plt.show()
 
 #------------
-def help(s):
-#------------ 
+def help(s=None):
+#------------
   if s:
-       print("Missing argument:")
        print(s)
 
   print("Usage example:")
   print("python xgb.py --hours 24 --company 3666 --begin 2020-12-01 --end 2021-12-05")
+  print("--------")
   exit(0)
 #---------------
 def main():
@@ -534,45 +552,48 @@ def main():
         help("")
 
     try:
-       args = input_cmd() 
+       args = input_cmd()
        print(args)
-       
+
        if not args.company:
            help("no company")
        if not  args.begin:
-           help("no begin")    
+           help("no begin")
        if not  args.end:
-           help("no end")    
+           help("no end")
        if not  args.hours:
-           help("no hours") 
+           help("no hours")
 
        company = args.company
        table = args.table
        begin = args.begin
        end = args.end
        f_steps = args.hours
+       direction = args.direction
 
        d_begin = datetime.strptime(begin,'%Y-%m-%d')
        d_end = datetime.strptime(end,'%Y-%m-%d')
        n_days = ( d_end - d_begin ).days
        print ("n_days=", n_days)
+       if args.hours *1.0 / (n_days * 24.0) > 0.5 :
+           print("The training interval too sort the asked predition hours is too long")
+           exit(1)
 
     except:
        print ("Error in parsing input")
        e = sys.exc_info()[0]
        print(e)
+       help()
        exit(1)
 
 
     if  int(company) >= 0:
         datastore.connect()
-        start_date=begin  
-        end_date=end  
-        direction=None # TODO read from command line
-        data = get_data_from_db(table, start_date, end_date, company, direction)
+        if direction not in [1,2]: direction=None #All directions
+        data = get_data_from_db(table, begin, end, company, direction)
         if data.empty:
             print("No data found")
-            return
+            exit(0)
 
 
         print(data)
@@ -588,17 +609,20 @@ def main():
 
     else:   # simulate input
         s = generate_series("linear_and_sin", begin,  n_days)
- 
+
     predictions=[]
-    options = ["use_shift","use_day_of_week","separate_col"]
+    options = ["use_lag_4", "use_shift","use_day_of_week"]  # ,"separate_col"]
     for option in options:
        p = multi_direct(s, f_steps, option)
        predictions.append(p)
-    
+
+    header = "Traffic (MB). "
     header=str(f_steps)+ " hours prediction. "
-    if int(company) >=0 and table : 
-            header = header + " Company " + str(company) +  " Table:" + table 
-        
+    if int(company) >=0 and table :
+            header = header + " Company " + str(company) +  ". Table: " + table
+            if direction:
+                header = header + " . Direction="+str(direction)
+
     final_plot(s, predictions, options, header)
 
 if __name__ == "__main__":
