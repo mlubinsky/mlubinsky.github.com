e633513d8688c542f9ed0515bb63e52457420ee5
Michael Lubinsky <mlubinsky@hotmail.com>
2021-10-29 23:18:01 -0700

    Create string.py

---
+++ interview/string.py
@@ -0,0 +1,94 @@
+# https://medium.com/hackernoon/14-patterns-to-ace-any-coding-interview-question-c5bb3357f6ed
+
+# Given a non-empty string s, you may delete at most one character. 
+# Judge whether you can make it a palindrome.
+# The string will only contain lowercase characters a-z.
+
+s = 'radkar'
+def solution(s):
+    for i in range(len(s)):
+        t = s[:i] + s[i+1:]
+        if t == t[::-1]: return True
+
+    return s == s[::-1]
+  
+solution(s)
+
+# Given an array of integers, determine whether the array is monotonic or not.
+A = [6, 5, 4, 4] 
+B = [1,1,1,3,3,4,3,2,4,2]
+C = [1,1,2,3,7]
+
+def solution(nums): 
+    return (all(nums[i] <= nums[i + 1] for i in range(len(nums) - 1)) or 
+            all(nums[i] >= nums[i + 1] for i in range(len(nums) - 1))) 
+  
+print(solution(A)) 
+print(solution(B)) 
+print(solution(C))
+
+
+#Given an array nums, write a function to move all zeroes to the end of it while maintaining the relative order of 
+#the non-zero elements.
+
+array1 = [0,1,0,3,12]
+array2 = [1,7,0,0,8,0,10,12,0,4]
+
+def solution(nums):
+    for i in nums:
+        if 0 in nums:
+            nums.remove(0)
+            nums.append(0)
+    return nums
+
+solution(array1)
+solution(array2)
+
+# Given an array containing None values fill in the None values with most recent 
+# non None value in the array 
+array1 = [1,None,2,3,None,None,5,None]
+
+def solution(array):
+    valid = 0            
+    res = []                 
+    for i in nums: 
+        if i is not None:    
+            res.append(i)
+            valid = i
+        else:
+            res.append(valid)
+    return res
+
+solution(array1)
+
+# Given two sentences, return an array that has the words that appear in one sentence and not
+# the other and an array with the words in common. 
+
+sentence1 = 'We are really pleased to meet you in our city'
+sentence2 = 'The city was hit by a really heavy storm'
+
+def solution(sentence1, sentence2):
+    set1 = set(sentence1.split())
+    set2 = set(sentence2.split())
+    
+    return sorted(list(set1^set2)), sorted(list(set1&set2)) # ^ A.symmetric_difference(B), & A.intersection(B)
+
+print(solution(sentence1, sentence2))
+
+
+# Given k numbers which are less than n, return the set of prime number among them
+# Note: The task is to write a program to print all Prime numbers in an Interval.
+# Definition: A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself. 
+
+n = 35
+def solution(n):
+    prime_nums = []
+    for num in range(n):
+        if num > 1: # all prime numbers are greater than 1
+            for i in range(2, num):
+                if (num % i) == 0: # if the modulus == 0 is means that the number can be divided by a number preceding it
+                    break
+            else:
+                prime_nums.append(num)
+    return prime_nums
+solution(n)
